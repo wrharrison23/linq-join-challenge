35512f537ada59188adc3bb105d3469ceb696447
Wes <wrharrison23@gmail.com>
2021-04-21 11:11:03 -0400

    finished challenge

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_cross_join
+{
+    /*
+    TASK:
+    As in the previous exercise, you're going to output the millionaires,
+    but you will also display the full name of the bank. You also need
+    to sort the millionaires' names, ascending by their LAST name.
+
+    Example output:
+        Tina Fey at Citibank
+        Joe Landy at Wells Fargo
+        Sarah Ng at First Tennessee
+        Les Paul at Wells Fargo
+        Peg Vale at Bank of America
+*/
+
+// Define a bank
+public class Bank
+{
+    public string Symbol { get; set; }
+    public string Name { get; set; }
+}
+
+// Define a customer
+public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
+
+public class Program
+{
+    public static void Main() {
+        // Create some banks and store in a List
+        List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+        };
+
+        // Create some customers and store in a List
+        List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+      /*
+          You will need to use the `Where()`
+          and `Select()` methods to generate
+          instances of the following class.
+
+          public class ReportItem
+          {
+              public string CustomerName { get; set; }
+              public string BankName { get; set; }
+          }
+      */
+      var millionaireReport = (from c in customers.Where(b => b.Balance >= 1000000)
+                                           join b in banks
+                                           on c.Bank equals b.Symbol
+                                           select new
+                                           {
+                                             CustomerName = c.Name,
+                                             BankName = b.Name
+                                           }).OrderBy(n => (n.CustomerName.Substring(n.CustomerName.IndexOf(" ") + 1))).ToList();
+
+      foreach (var item in millionaireReport)
+        {
+            Console.WriteLine($"{item.CustomerName} at {item.BankName}");
+        }
+    }
+    public class ReportItem
+            {
+                public string CustomerName { get; set; }
+                public string BankName { get; set; }
+            }
+}
+}
